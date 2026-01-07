b8648296d57dd8f0430d9075c180c18603abb5df
sujit singh <sujitsingh@sujits-MacBook-Air.local>
2026-01-07 16:21:44 +0800

    Force light mode - disable dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

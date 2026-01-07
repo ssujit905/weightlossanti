76aa9f67a782bfdd302482a863b57e330fdc4e1d
sujit singh <sujitsingh@sujits-MacBook-Air.local>
2026-01-07 16:22:59 +0800

    Revert "Force light mode - disable dark theme"
    This reverts commit b8648296d57dd8f0430d9075c180c18603abb5df.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
